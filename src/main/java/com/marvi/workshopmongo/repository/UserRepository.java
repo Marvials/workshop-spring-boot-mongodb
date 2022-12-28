4ce58172bd0ba93991202448eeb9a14b1fc2452b
Marvials <marcosgaba7@gmail.com>
2022-12-27 21:22:59 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/marvi/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.marvi.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marvi.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
